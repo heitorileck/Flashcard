8584752fe5cfcc2dd2d51c3d3533036d9e7a8175
heitorileck <00001075687305sp@aluno.educacao.sp.gov.br>
2024-11-04 18:12:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Programação',
+    'O que é Phython?',
+    'O Javascript é uma línguagem de programação'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que execulta uma tarefa'
+)
+
+criaCartao (
+'Lingua inglesa',
+'Como se diz oi em Inglês?',
+'Oi em inglês é HI (RAI)'
+)
